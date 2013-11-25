b137401d5c2c91dad31362b3d927f53f501fdda1
Yuhua Mai <yuhua.mai@duke.edu>
2013-11-25 18:03:05 -0500

    Initial Commit

---
+++ Music/YMViewController.h
@@ -0,0 +1,13 @@
+//
+//  YMViewController.h
+//  Music
+//
+//  Created by Yuhua Mai on 11/25/13.
+//  Copyright (c) 2013 Yuhua Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMViewController : UIViewController
+
+@end
---
+++ Music/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Music
+//
+//  Created by Yuhua Mai on 11/25/13.
+//  Copyright (c) 2013 Yuhua Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YMAppDelegate class]));
+    }
+}
