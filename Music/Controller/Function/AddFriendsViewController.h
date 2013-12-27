013b48c6187e3a7736d8e195d886020d6cb8e79f
myhgew <yuhua.mai@duke.edu>
2013-12-27 12:01:11 -0500

    get list of contacts

---
+++ Music/Controller/Function/AddFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendsViewController.h
+//  Beet
+//
+//  Created by Yuhua Mai on 12/27/13.
+//  Copyright (c) 2013 Yuhua Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendsViewController : UIViewController
+
+@end
