77b485214899dda8e4b8ead742272f499c6fa815
myhgew <yuhua.mai@duke.edu>
2014-01-20 21:17:14 -0500

    Setting - change username, email, phonenumber

--- Avibe/Controller/Setting/SettingViewController.m
+++ Avibe/Controller/Setting/SettingViewController.m
@@ -57,6 +57,7 @@
     [scrollView setContentSize:CGSizeMake(scrollWidth, scrollHeight)];
     scrollView.userInteractionEnabled = YES;
     self.view = scrollView;
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyBoard)]];
     
     //BackgroundView
     UIView *backgroundView = [[BackgroundImageView alloc] initWithFrame:CGRectMake(0, 0, scrollWidth, scrollHeight)];
@@ -64,7 +65,7 @@
     [scrollView sendSubviewToBack:backgroundView];
     
     /*AccountView*/
-    item = 3;
+    item = 4;
     currentHeight += barHeight;
     accountView = [[UIView alloc] initWithFrame:CGRectMake(0, currentHeight, width, item*unitHeight)];
     accountView.backgroundColor = contentBackgroundColor;
@@ -84,26 +85,46 @@
     contentLabel.textAlignment = NSTextAlignmentNatural;
     [accountView addSubview:contentLabel];
     //User Name Description
-    contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(width/2, unitHeight, width/2-right, unitHeight)];
-    contentLabel.backgroundColor = contentBackgroundColor;
-    contentLabel.text = @" myhgew ";
-    contentLabel.textColor = contentTextColor;
-    contentLabel.textAlignment = NSTextAlignmentRight;
-    [accountView addSubview:contentLabel];
-    //Mobile#
+    textField = [[UITextField alloc] initWithFrame:CGRectMake(width/2, unitHeight, width/2-right, unitHeight)];
+    textField.text = [[PFUser currentUser] username];
+    textField.backgroundColor = contentBackgroundColor;
+    textField.textAlignment = NSTextAlignmentRight;
+    textField.delegate = self;
+//    [textField addTarget:self action:@selector(changeUsername:) forControlEvents:UIControlEventEditingDidEnd];
+    [accountView addSubview:textField];
+    
+    //Email
     contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, unitHeight*2, width/2, unitHeight)];
+    contentLabel.backgroundColor = contentBackgroundColor;
+    contentLabel.text = @" Email";
+    contentLabel.textColor = contentTextColor;
+    contentLabel.textAlignment = NSTextAlignmentNatural;
+    [accountView addSubview:contentLabel];
+    //Email Description
+    textField = [[UITextField alloc] initWithFrame:CGRectMake(width/2, unitHeight*2, width/2-right, unitHeight)];
+    textField.text = [[PFUser currentUser] objectForKey:kClassUserEmail];
+    textField.backgroundColor = contentBackgroundColor;
+    textField.textAlignment = NSTextAlignmentRight;
+    textField.delegate = self;
+    textField.adjustsFontSizeToFitWidth = YES;
+    [textField addTarget:self action:@selector(changeEmail:) forControlEvents:UIControlEventEditingDidEnd];
+    [accountView addSubview:textField];
+    
+    //Mobile#
+    contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, unitHeight*3, width/2, unitHeight)];
     contentLabel.backgroundColor = contentBackgroundColor;
     contentLabel.text = @" Mobile#";
     contentLabel.textColor = contentTextColor;
     contentLabel.textAlignment = NSTextAlignmentNatural;
     [accountView addSubview:contentLabel];
     //Mobile# Description
-    contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(width/2, unitHeight*2, width/2-right, unitHeight)];
-    contentLabel.backgroundColor = contentBackgroundColor;
-    contentLabel.text = @" 9193082709 ";
-    contentLabel.textColor = contentTextColor;
-    contentLabel.textAlignment = NSTextAlignmentRight;
-    [accountView addSubview:contentLabel];
+    textField = [[UITextField alloc] initWithFrame:CGRectMake(width/2, unitHeight*3, width/2-right, unitHeight)];
+    textField.text = [[PFUser currentUser] objectForKey:kClassUserPhoneNumber];
+    textField.backgroundColor = contentBackgroundColor;
+    textField.textAlignment = NSTextAlignmentRight;
+    textField.delegate = self;
+    [textField addTarget:self action:@selector(changePhoneNumber:) forControlEvents:UIControlEventEditingDidEnd];
+    [accountView addSubview:textField];
     
     /*OtherAccountView*/
     currentHeight += item*unitHeight;
@@ -151,6 +172,66 @@
     [accountView addSubview:titleLabel];
 }
 
+#pragma mark - Textfield Method
+- (void)changeUsername:(UITextField*)sender
+{
+    NSString *username = sender.text;
+    
+    PFQuery *query = [PFUser query];
+    [query getObjectInBackgroundWithId:[[PFUser currentUser] objectId] block:^(PFObject *object, NSError *error) {
+        if(object){
+            [object setObject:username forKey:kClassUserUsername];
+            [object saveEventually:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [[[UIAlertView alloc] initWithTitle: @"Success" message: [NSString stringWithFormat:@"You have changed username to %@.  Please sign in again to see the update.", username] delegate: self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                }else if(error){
+                    [[[UIAlertView alloc] initWithTitle: @"Error" message:[NSString stringWithFormat:@"You can't change to %@, please try another one", username] delegate: self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                    sender.text = [[PFUser currentUser] username];
+                }
+            }];
+        }
+    }];
+}
+- (void)changeEmail:(UITextField*)sender
+{
+    NSString *email = sender.text;
+    
+    PFQuery *query = [PFUser query];
+    [query getObjectInBackgroundWithId:[[PFUser currentUser] objectId] block:^(PFObject *object, NSError *error) {
+        if(object){
+            [object setObject:email forKey:kClassUserEmail];
+            [object saveEventually:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [[[UIAlertView alloc] initWithTitle: @"Success" message: [NSString stringWithFormat:@"You have changed email address to %@. Please sign in again to see the update.", email] delegate: self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                }else if(error){
+                    [[[UIAlertView alloc] initWithTitle: @"Error" message:[NSString stringWithFormat:@"You can't change email address to %@, please try another one", email] delegate: self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                    sender.text = [[PFUser currentUser] objectForKey:kClassUserEmail];
+                }
+            }];
+        }
+    }];
+}
+- (void)changePhoneNumber:(UITextField*)sender
+{
+    NSString *phoneNumber = sender.text;
+    
+    PFQuery *query = [PFUser query];
+    [query getObjectInBackgroundWithId:[[PFUser currentUser] objectId] block:^(PFObject *object, NSError *error) {
+        if(object){
+            [object setObject:phoneNumber forKey:kClassUserPhoneNumber];
+            [object saveEventually:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [[[UIAlertView alloc] initWithTitle: @"Success" message: [NSString stringWithFormat:@"You have changed phone number to %@. Please sign in again to see the update.", phoneNumber] delegate: self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                }else if(error){
+                    [[[UIAlertView alloc] initWithTitle: @"Error" message:[NSString stringWithFormat:@"You can't change phone number to %@, please try another one", phoneNumber] delegate: self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                    sender.text = [[PFUser currentUser] objectForKey:kClassUserPhoneNumber];
+                }
+            }];
+        }
+    }];
+}
+
+
 #pragma mark - BarMenuButton
 -(void)setupBarMenuButton{
     //Navigation Title
@@ -181,6 +262,11 @@
     [[Setting sharedSetting] setLastFMAccount:account];
 }
 
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+}
+
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     //hide the keyboard
@@ -195,19 +281,19 @@
 }
 
 #pragma mark - Touch
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+- (void)hideKeyBoard
 {
-    for (UIView * txt in self.view.subviews){
-        if ([txt isKindOfClass:[UITextField class]] && [txt isFirstResponder]) {
-            UITextField *textField = (UITextField*)txt;
-
-            if (textField == _lastFMAccountTextField) {
-                [self setLastFMAccount:textField.text];
+    for (UIView *view1 in self.view.subviews){
+        for(UIView *view2 in view1.subviews){
+            if ([view2 isKindOfClass:[UITextField class]] && [view2 isFirstResponder]) {
+                UITextField *textField = (UITextField*)view2;
+                [textField resignFirstResponder];
+                return;
             }
-            
-            [txt resignFirstResponder];
         }
     }
 }
+
+
 
 @end
