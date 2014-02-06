0f3620eb374c630ac88a1a6923e7fb18149ca43e
myhgew <yuhua.mai@duke.edu>
2014-02-05 23:56:50 -0500

    fix bug notification in background

--- Avibe/Controller/Welcome/AppDelegate.m
+++ Avibe/Controller/Welcome/AppDelegate.m
@@ -22,6 +22,7 @@
 
 @interface AppDelegate()
 @property (nonatomic, strong) SaveMusicFromSources *saveMusic;
+@property (nonatomic, strong) MPMusicPlayerController *player;
 @end
 
 @implementation AppDelegate
@@ -74,13 +75,13 @@
     
     /*Notification*/
     NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
-    MPMusicPlayerController *player = [MPMusicPlayerController iPodMusicPlayer];
+    _player = [MPMusicPlayerController iPodMusicPlayer];
     
     [notificationCenter addObserver:self
                            selector:@selector(nowPlayingItemChanged:)
                                name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification
-                             object:player];
-    [player beginGeneratingPlaybackNotifications];
+                             object:_player];
+    [_player beginGeneratingPlaybackNotifications];
     
     return YES;
 }
@@ -110,6 +111,7 @@
     //fetch the latest content
 }
 
+#pragma mark - Notification
 -(void)nowPlayingItemChanged:(NSNotification *)notification {
     MPMusicPlayerController *player = (MPMusicPlayerController *)notification.object;
     MPMediaItem *song = [player nowPlayingItem];
@@ -152,38 +154,27 @@
     
 }
 
+#pragma mark - Application Method
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [_player endGeneratingPlaybackNotifications];
+}
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [_player beginGeneratingPlaybackNotifications];
+}
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    /*Notification*/
-    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
-    MPMusicPlayerController *player = [MPMusicPlayerController iPodMusicPlayer];
-    
-    [notificationCenter addObserver:self
-                           selector:@selector(nowPlayingItemChanged:)
-                               name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification
-                             object:player];
-    [player beginGeneratingPlaybackNotifications];
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
-
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
