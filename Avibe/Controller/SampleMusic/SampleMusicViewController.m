fd455276f34d39dc7b518ee5483ff81c785951b4
myhgew <yuhua.mai@duke.edu>
2014-02-28 21:00:27 -0500

    Youtube Player API

--- Avibe/Controller/SampleMusic/SampleMusicViewController.m
+++ Avibe/Controller/SampleMusic/SampleMusicViewController.m
@@ -515,12 +515,21 @@
     _infoLabel.text = title;
     
     [_spinner stopAnimating];
+    
     [self playYoutube:href];
+//    [self playYoutubeIFrame:href];
 }
 - (void)fetchedDataWithError
 {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Error" message: @"Sorry, can't listen in Youtube right now. Please try later." delegate:self.delegate cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [alert show];
+}
+- (void)playYoutubeIFrame:(NSString*)videoUrl
+{
+    /**
+     * https://developers.google.com/youtube/iframe_api_reference
+     */
+    NSString* embedHTML = [NSString stringWithFormat:@"<!DOCTYPE html> <html> <body>  <div id=\"player\"></div> <script> var tag = document.createElement('script');  tag.src = \"https://www.youtube.com/iframe_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubeIframeAPIReady() { player = new YT.Player('player', { height: '390', width: '640', videoId: 'M7lc1UVf-VE', events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange } }); } function onPlayerReady(event) {  event.target.playVideo(); } var done = false; function onPlayerStateChange(event) { if (event.data == YT.PlayerState.PLAYING && !done) { setTimeout(stopVideo, 6000); done = true; } } function stopVideo() { player.stopVideo(); } </script> </body>  </html>", videoUrl];
 }
 - (void)playYoutube:(NSString*)videoURL
 {
